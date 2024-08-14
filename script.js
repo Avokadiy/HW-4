50069e36de0270bf0067d8828df5e9e872e82986
Никита Демидов <braaedy1@mail.ru>
2024-08-14 17:54:51 +0500

    final ver.2

--- script.js
+++ script.js
@@ -9,20 +9,27 @@ import {calculateAverageNumber,showAverageNumber} from "./modules/calculateAvera
 import {concatStrings, showConcatStrings} from "./modules/concatStrings.js"
 
 const myName = "Никита";
+greet(myName);
+
 let celsius = prompt('Укажите градусы Цельсия', 15);
 let celsiusConvRes = calcCelsiusToFahrenheit(celsius);
+showFahrenheit(celsiusConvRes);
+
 let time = prompt('Время падения объекта в секунду', 999);
 let distance = calculateFallDistance(time);
+showFallDistance(distance);
+
 let firstNumber = Number(prompt('Введите первое число'));
 let secondNumber = Number(prompt('Введите второе число'));
 let thirdNumber = Number(prompt('Введите третье число'));
 let averageNumberRes = calculateAverageNumber(firstNumber, secondNumber, thirdNumber);
+showAverageNumber(averageNumberRes);
+
 let firstWord = prompt('Введите первое слово');
 let secondWord = prompt('Введите второе слово');
 let concatStringsRes = concatStrings(firstWord, secondWord);
-
-greet(myName);
-showFallDistance(distance);
-showFahrenheit(celsiusConvRes);
-showAverageNumber(averageNumberRes);
 showConcatStrings(concatStringsRes);
+
+
+
+
